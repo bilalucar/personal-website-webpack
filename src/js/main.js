c7f595b58245b1da8b1e3730e1b7ee5f4fd56ca3
Bilal Uçar <bilal.ucar@sahabt.com>
2019-06-30 22:12:00 +0300

    Content on the home page has been updated. Added animation to sections.

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,12 @@ import 'bootstrap/js/src/tab';
 import 'bootstrap/js/src/util';
 import 'bootstrap/js/src/collapse';
 import 'bootstrap/js/src/dropdown';
+import WOW from 'wow.js';
+
+const wow = new WOW({
+    boxClass:     'wow',
+    animateClass: 'animated',
+    offset:       0,
+    mobile:       true,
+    live:         true
+});
